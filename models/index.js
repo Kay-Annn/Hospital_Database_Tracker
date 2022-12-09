f72637d425eebb1fd7a97a3027a069a6a8b574b6
Kush Patel <kushrpatel06@yahoo.com>
2022-12-08 20:12:36 -0500

    More routing Added biolerplate for both index.js files inside the controllers folder Added boilerplate for index.js file inside the models folder (so future table relations)

---
+++ models/index.js
@@ -0,0 +1,8 @@
+//imports
+const Floor = require("./Floor");
+const User = require("./User");
+//insert other table class model imports
+
+//table relations
+
+module.exports = {Floor, User /*, ... */};
