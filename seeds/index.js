b76228acbbecb0cede5a069619033dd84e1d6ae5
Kay <ckay_006@yahoo.com>
2022-12-08 19:00:34 -0500

    added model and seed info for user table

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,14 @@
 //imports
 const sequelize = require("../config/connection");
 const seedFloor = require("./floorData");
+const seedUser = require("./userData");
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
 
     await seedFloor();
+
+    await seedUser();
 
     process.exit(0);
 };
