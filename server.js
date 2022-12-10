60ebb0842f5908ecd0502ca4352f47df2d469c6c
Kay <ckay_006@yahoo.com>
2022-12-10 00:52:58 -0500

    added login route installed bcrypt package started login ui

--- server.js
+++ server.js
@@ -2,14 +2,13 @@
 const path = require("path");
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
-const routes = require("./controllers");
+const routes = require("./controllers/index.js");
 const helpers = require("./utils/helpers");
-/*           possible session import            */ 
-//db related imports
 const sequelize = require("./config/connection");
 /*           possible session store import            */
 //app and port
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 //setup handlebars engine with custom helps support
 const handlebars = expressHandlebars.create({ helpers });
@@ -30,9 +29,13 @@ app.set("view-engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 //turn on routes
 app.use(routes);
 //start server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}` ));
   });
