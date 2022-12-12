025a84a4f6a831ac8c455ed224ad8d26afb2953c
tsy0103 <111820850+tsy0103@users.noreply.github.com>
2022-12-12 14:52:37 -0500

    update employee js
    update

--- controllers/api/employee.js
+++ controllers/api/employee.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    //判断是否有cookie
+    //do we have cookie
     console.log('%c cookies ', 'color:#fff; background:#00897b ')
     console.log(req.cookies)
 
@@ -21,7 +21,7 @@ router.get('/login', function (req, res, next) {
     res.sendFile(path.join(__dirname, '../../public/login.html'));
 });
 router.post('/login', async function (req, res, next) {
-    console.log('%c 开始测试 ', 'color:#fff; background:#00897b ')
+    console.log('%c Start Testing ', 'color:#fff; background:#00897b ')
     console.log(req.body);
     let username = req.body.name || '';
     let password = req.body.password || '';
@@ -31,36 +31,36 @@ router.post('/login', async function (req, res, next) {
             where: {username: username, password: password, role: role}
         });
         if (users.length > 0) {
-            res.send({code: 500, msg: '已存在此人'});
+            res.send({code: 500, msg: 'User Exist!'});
         } else {
             const user = await employee.create({
                 username: username, password: password, role: role
             });
             res.cookie('isLogin', '1', {maxAge: 60 * 60 * 1000});
-            res.send({msg: '注册成功', code: 200});
+            res.send({msg: 'Success!', code: 200});
         }
         return
     }
 
     if (username && password) {
-        //查询数据库有无此人
+        //if we have that user or not
         const users = await employee.findAll({
             where: {username: username, password: password}
         });
         if (users.length > 0) {
-            //设置网页cookie 有效期为1小时
+            //set cookie to 1hr
             res.cookie('isLogin', '1', {maxAge: 60 * 60 * 1000});
-            res.send({code: 200, msg: '登录成功'});
+            res.send({code: 200, msg: 'Success'});
         } else {
-            res.send({msg: '用户名或密码错误'});
+            res.send({msg: 'Username or password incorrect'});
         }
     } else {
-        res.send({msg: '用户名或密码错误'});
+        res.send({msg: 'User name or password incorrect'});
     }
 });
 
 
-//读取全部数据
+//read all data
 router.get('/read', async function (req, res, next) {
     const users = await employee.findAll();
     res.send(users);
