d61c2cc38438fecbf9e358daa054eb90c6dd3dc9
Kay <ckay_006@yahoo.com>
2022-12-13 21:05:42 -0500

    added log out functionality

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -92,5 +92,16 @@ router.post('/signupUser', async (req, res) => {
 });
 
 
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    // Remove the session variables
+    req.session.destroy(() => {
+      res.status(200).end();
+    });
+
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
